9e9700c7a1b935bac46b59578a72621ce47c8e25
tiagofcvale <tiagofcvale@ua.pt>
2025-10-22 17:31:15 +0100

    test

--- BernardoTiago_RecycleBin/test_suite.sh
+++ BernardoTiago_RecycleBin/test_suite.sh
@@ -110,7 +110,7 @@ test_restore_file
 # Add more test functions here 
  
 teardown 
- 
+
 echo "=========================================" 
 echo "Results: $PASS passed, $FAIL failed" 
 echo "========================================="
